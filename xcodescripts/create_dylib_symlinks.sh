21f1a6353fbf611ef5ec78ba2af3221d2d98b249
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2020-01-31 01:57:31 +0000

    Libsystem-1281
    Imported from Libsystem-1281.tar.gz

--- xcodescripts/create_dylib_symlinks.sh
+++ xcodescripts/create_dylib_symlinks.sh
@@ -5,11 +5,17 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
+if [[ "${DRIVERKIT}" == "1" ]]; then exit 0; fi; # No compatibility symlinks in DriverKit SDK
+
 DSTROOT="$1${INSTALL_PATH_PREFIX}"
 ACTION="$2"
 VARIANTS="$3"
 
 BSD_LIBS="c info m pthread dbm poll dl rpcsvc proc"
+
+if [[ "${PLATFORM_NAME}" =~ macosx ]]; then
+   BSD_LIBS="${BSD_LIBS} gcc_s.1"
+fi
 
 mkdir -p "${DSTROOT}/usr/lib" || exit 1
 
@@ -20,11 +26,7 @@ if [ "${ACTION}" != "installhdrs" ]; then
 	    suffix="_${variant}"
 	fi
 
-        if [[ "${PLATFORM_NAME}" =~ simulator ]] ; then
-            ln -sf "libSystem${suffix}.dylib" "${DSTROOT}/usr/lib/libSystem.B${suffix}.dylib" || exit 1
-        else
-            ln -sf "libSystem.B${suffix}.dylib" "${DSTROOT}/usr/lib/libSystem${suffix}.dylib" || exit 1
-        fi
+        ln -sf "libSystem.B${suffix}.dylib" "${DSTROOT}/usr/lib/libSystem${suffix}.dylib" || exit 1
 
 	for i in ${BSD_LIBS}; do
 	    ln -sf "libSystem.dylib" "${DSTROOT}/usr/lib/lib${i}.dylib" || exit 1
